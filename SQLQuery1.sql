240a34d39c6c4df05a60414c2331c949f23b4a56
johanssonida1996 <70290036+johanssonida1996@users.noreply.github.com>
2021-02-19 11:38:48 +0100

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE SchoolClasses (
+	Id uniqueidentifier not null primary key,
+	ClassName nvarchar(20) not null,
+	TeacherId nvarchar(450)
+)
+GO
+CREATE TABLE SchoolClassStudents (
+	StudentId nvarchar(450) not null primary key,
+	SchoolClassId uniqueidentifier not null references SchoolClasses(Id)
+)
+GO
